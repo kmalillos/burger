781f4eb487bbcb9334f8fc72bec68d52005097f7
kmalillos <malillosk@gmail.com>
2019-01-13 21:33:50 -0700

    added styles

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,5 @@
 INSERT INTO burgers (burger_name) VALUES ("Big Mac");
 INSERT INTO burgers (burger_name) VALUES ("Whooper");
+INSERT INTO burgers (burger_name) VALUES ("BBQ Burger");
 INSERT INTO burgers (burger_name, devoured) VALUES ("Double Cheese Burger", true);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Mushroom Swiss Burger", true);
